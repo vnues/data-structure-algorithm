5fdc64d6738b7bdf4d1d6f763289e80243c7da60
vnues <1518190293@qq.com>
2021-06-11 17:10:26 +0800

    update

--- 82. 删除排序链表中的重复元素 II.js
+++ 82. 删除排序链表中的重复元素 II.js
@@ -9,41 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// [1,2,3,3,4,4,5]
-// 这个双指针太恶心了 
-// var deleteDuplicates = function(head) {
-//   // 做题之前 先处理边界条件
-//   if(!head){
-//     return head
-//   }
-//   const list=new ListNode(-1)
-//   let listHead=list
-//   // 有序链表 双指针
-//   let slow=head
-//   let fast=head.next
-//   while(fast&&slow){
-//     if(slow.val===fast.val){
-//       while(fast&&fast.next&&slow.val===fast.val){
-//         fast=fast.next
-//       }
-//       slow=fast
-//     } 
-//     listHead.next= new ListNode(slow.val)
-//     listHead=listHead.next
-//     slow=slow.next
-//     fast=fast.next
-//   }
-//   return list.next
-// };
-
 
 var deleteDuplicates = function(head) {
   if (!head) {
       return head;
   }
-
+  // 其实就是快慢指针
+  // cur.next ==>slow指针
+  // cur.next.next ==>fast指针
   const dummy = new ListNode(0, head); // 哨兵
-
   let cur = dummy;
   while (cur.next && cur.next.next) {
       if (cur.next.val === cur.next.next.val) {
